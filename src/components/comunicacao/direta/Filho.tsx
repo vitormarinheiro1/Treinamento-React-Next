80a07c9c88caa8642526e15099c34aeea4df37f2
unknown <vitormarinheiro9@gmail.com>
2023-06-28 20:51:08 -0300

    Arvore de componentes

---
+++ src/components/comunicacao/direta/Filho.tsx
@@ -0,0 +1,21 @@
+interface FilhoProps {
+    nome: string
+    sobrenome:string
+}
+
+export default function Filho(props: FilhoProps) {
+    
+    return(
+        <div className={`
+            flex justify-center items-center
+            bg-green-500 rounded-md p-5 border border-white
+        `}>
+            <div className="flex justify-center gap-1 text-xl">
+                <span className="font-black">Filho</span>
+                <span>{props.nome}</span>
+                <span>{props.sobrenome}</span>
+            </div>
+        </div>   
+   
+    )
+}
--- src/components/hooks/ImagemAleatoria.tsx
+++ src/components/hooks/ImagemAleatoria.tsx
@@ -1,22 +1,55 @@
+import Image from "next/image"
+import { useState } from "react"
+
 export default function ImagemAleatoria(){
+    
+    const [pesquisa, alterarPesquisa] = useState<string>('person')
+    // let pesquisa: string = ''
 
-    const url = 'https://source.unsplash.com/featured/300x300'
+    const [tamanho, alterarTamanho] = useState<number>(300)
 
-    let pesquisa: string = ''
+    const url = 'https://source.unsplash.com/featured/'
 
-    function renderizarBotao() {
+    function renderizarBotao(valor: string) {
         return (
             <button className={`
                 bg-blue-500 px-4 py-2 rounded-md
-            `}>
-                Botão
+            `} onClick={() => {
+                alterarPesquisa(valor)
+            }}>
+                {valor}
             </button>    
         )
     }
 
     return (
-        <div>
-            {renderizarBotao()}
+        <div className="flex flex-col gap-3 border border-zinc-500 p-7 rounded-md">
+            <div className="flex justify-center gap-7 mb-5">
+                <span>{pesquisa}</span>
+                <span>{tamanho}x{tamanho}</span>
+            </div>
+            <Image 
+            src={`${url}${tamanho}x${tamanho}?${pesquisa}`} 
+            height={300} width={300} 
+            alt="Imagem"
+            className="rounded-md"
+            />
+            <div className={`flex justify-between gap-5`}>
+                {renderizarBotao('abstract')}
+                {renderizarBotao('city')}
+                {renderizarBotao('person')}
+            </div>
+            <div>
+                <input 
+                type="number" 
+                value={tamanho} 
+                className="bg-zinc-800 p-2 rounded-md outline-none w-full"
+                onChange={e => {
+                    alterarTamanho(+e.target.value)
+                }}
+                />
+            </div>
+
         </div>
         )
 }
